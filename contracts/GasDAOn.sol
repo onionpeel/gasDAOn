2cb4947b03f35884c0954ac712d82ff8df41e56d
Jeff Dinkelman <mfankenstein@yahoo.com>
2022-04-09 14:46:10 -0700

    comments

--- contracts/GasDAOn.sol
+++ contracts/GasDAOn.sol
@@ -3,11 +3,19 @@ pragma solidity 0.8.0;
 
 import "@openzeppelin/contracts/utils/Counters.sol";
 
+/* @notice Built for the EthGlobal 2022 DAOHacks hackathon
+  @notice A participation/voting contract without on-chain enforcement. Data about participation, proposals and votes is emitted as events and can be collated using an indexer.  Any disputes about vote tallies can be resolved by referring to the events since they are immutable.  The rules for using this contract are in the comments as opposed to the normal practice, which puts them in the code.  While this approach leaves the contract exposed to leaks (ex, participants can lie about their greenhouse gas emissions or clients can interpret voting rules differently), it offers a lightweight mechanism for those who want to participate but would otherwise be excluded due to gas costs.
+  @authoer: onionpeel
+  github: https://github.com/onionpeel/gasDAOn
+**/
+
 contract GasDAOn {
   using Counters for Counters.Counter;
 
   Counters.Counter public proposalId;
   mapping (proposalId => bool) proposals;
+
+
 
   enum GreenhouseGasUsage {
     PlanetaryArsonist,
@@ -30,7 +38,8 @@ contract GasDAOn {
   );
 
   event Vote(
-
+    uint256 indexed proposalId,
+    uint256 indexed vote
   );
 
   function participate(GreenhouseGasUsage _usage) external {
@@ -51,7 +60,12 @@ contract GasDAOn {
     );
   }
 
-  function vote() {
+  function vote(uint256 _proposalId, uint256 _vote) external {
+    require(_proposalId == true; "GasDAOn: proposalID does not exist");
 
+    emit Vote(
+      _proposalId,
+      _vote
+    );
   }
 }
