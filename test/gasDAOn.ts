e79aa5c84ad63536b4852a9e06fa86524dc03cba
Jeff Dinkelman <mfankenstein@yahoo.com>
2022-04-09 19:24:12 -0700

    finishes basic test

--- test/gasDAOn.ts
+++ test/gasDAOn.ts
@@ -43,45 +43,94 @@ describe('GasDAOn', () => {
 
     await gasDAOn.connect(fairShare1).propose("Would the world be better off if people used bicycles instead of cars?  No: 0, Yes: 1");
 
-    await gasDAOn.connect(planetaryArsonist1).vote(
+    await expect(gasDAOn.connect(planetaryArsonist1).vote(
       ethers.BigNumber.from('1'),
       ethers.BigNumber.from('0')
-    );
-    await gasDAOn.connect(planetaryArsonist2).vote(
+    )).to.emit(gasDAOn, 'Vote')
+      .withArgs(
+        ethers.BigNumber.from('1'),
+        ethers.BigNumber.from('0')
+      );
+
+    await expect(gasDAOn.connect(planetaryArsonist2).vote(
       ethers.BigNumber.from('1'),
       ethers.BigNumber.from('0')
-    );
-    await gasDAOn.connect(charburger1).vote(
+    )).to.emit(gasDAOn, 'Vote')
+      .withArgs(
+        ethers.BigNumber.from('1'),
+        ethers.BigNumber.from('0')
+      );
+
+    await expect(gasDAOn.connect(charburger1).vote(
       ethers.BigNumber.from('1'),
       ethers.BigNumber.from('0')
-    );
-    await gasDAOn.connect(charburger2).vote(
+    )).to.emit(gasDAOn, 'Vote')
+      .withArgs(
+        ethers.BigNumber.from('1'),
+        ethers.BigNumber.from('0')
+      );
+
+    await expect(gasDAOn.connect(charburger2).vote(
       ethers.BigNumber.from('1'),
       ethers.BigNumber.from('0')
-    );
-    await gasDAOn.connect(lowSimmer1).vote(
+    )).to.emit(gasDAOn, 'Vote')
+      .withArgs(
+        ethers.BigNumber.from('1'),
+        ethers.BigNumber.from('0')
+      );
+
+    await expect(gasDAOn.connect(lowSimmer1).vote(
       ethers.BigNumber.from('1'),
       ethers.BigNumber.from('0')
-    );
-    await gasDAOn.connect(lowSimmer2).vote(
+    )).to.emit(gasDAOn, 'Vote')
+      .withArgs(
+        ethers.BigNumber.from('1'),
+        ethers.BigNumber.from('0')
+      );
+
+    await expect(gasDAOn.connect(lowSimmer2).vote(
       ethers.BigNumber.from('1'),
       ethers.BigNumber.from('0')
-    );
-    await gasDAOn.connect(fairShare1).vote(
+    )).to.emit(gasDAOn, 'Vote')
+      .withArgs(
+        ethers.BigNumber.from('1'),
+        ethers.BigNumber.from('0')
+      );
+
+    await expect(gasDAOn.connect(fairShare1).vote(
       ethers.BigNumber.from('1'),
       ethers.BigNumber.from('1')
-    );
-    await gasDAOn.connect(fairShare2).vote(
+    )).to.emit(gasDAOn, 'Vote')
+      .withArgs(
+        ethers.BigNumber.from('1'),
+        ethers.BigNumber.from('1')
+      );
+
+    await expect(gasDAOn.connect(fairShare2).vote(
       ethers.BigNumber.from('1'),
       ethers.BigNumber.from('1')
-    );
-    await gasDAOn.connect(undisclosed1).vote(
+    )).to.emit(gasDAOn, 'Vote')
+      .withArgs(
+        ethers.BigNumber.from('1'),
+        ethers.BigNumber.from('1')
+      );
+
+    await expect(gasDAOn.connect(undisclosed1).vote(
       ethers.BigNumber.from('1'),
       ethers.BigNumber.from('0')
-    );
-    await gasDAOn.connect(undisclosed2).vote(
+    )).to.emit(gasDAOn, 'Vote')
+      .withArgs(
+        ethers.BigNumber.from('1'),
+        ethers.BigNumber.from('0')
+      );
+
+    await expect(gasDAOn.connect(undisclosed2).vote(
       ethers.BigNumber.from('1'),
       ethers.BigNumber.from('0')
-    );
+    )).to.emit(gasDAOn, 'Vote')
+      .withArgs(
+        ethers.BigNumber.from('1'),
+        ethers.BigNumber.from('0')
+      );
   });
 });
