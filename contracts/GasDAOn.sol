7fc93ba282d5b5228fd4017cfe5051a63d2390f2
Jeff Dinkelman <mfankenstein@yahoo.com>
2022-04-09 18:34:49 -0700

    comments

--- contracts/GasDAOn.sol
+++ contracts/GasDAOn.sol
@@ -15,18 +15,29 @@ contract GasDAOn {
   Counters.Counter public proposalId;
   mapping (proposalId => bool) proposals;
 
-  // duration: 1 week
+  uint256 public const votingPeriod = 604800
 
-  // weighting
+  /* weighting
+    1 / (Number of tons to be reduced + 1.4)
+    1.4 is added to the reduction because that is the amount by which the lowest emitting 50% of the population can increase their greenhouse gas emissions while still being within their fair share.
+    The voting power for undisclosed participants was set arbitrarily.
+  **/
+  uint256 public planetaryArsonist = 13;
+  uint256 public charburger = 45;
+  uint256 public lowSimmer = 250;
+  uint256 public fairShare = 1000;
+  uint256 public undisclosed = 20;
 
   /*
-  based on vox article
+  To estimate one's annual greenhouse gas emissions, use the data provided in:
+  https://www.vox.com/22291568/climate-change-carbon-footprint-greta-thunberg-un-emissions-gap-report
+  https://wedocs.unep.org/xmlui/bitstream/handle/20.500.11822/34432/EGR20ch6.pdf?sequence=3
   **/
   enum GreenhouseGasUsage {
-    PlanetaryArsonist,
-    Charburger,
-    LowSimmer,
-    FairShare,
+    PlanetaryArsonist, // annual income greater than 109,000 USD
+    Charburger, // annual income between 38,000 and 109.000 USD
+    LowSimmer, // annual income between 6,000 and 38,000 USD
+    FairShare, // annual income less than 6,000
     Undisclosed
   }
 
@@ -60,7 +71,7 @@ contract GasDAOn {
 
   /**
     @notice Only proposals made by participants are valid
-    @dev The proposal should be in the form of a question.
+    @dev The proposal should be in the form of a question with numeric options.
     Binary voting:
     "Should this DAO approve the findings of the IPCC Sixth Assessment Report, which was finalized on 4 April 2022?  No: 0, Yes: 1"
     Non-binary voting:
@@ -81,8 +92,11 @@ contract GasDAOn {
   /*
     @notice Participants vote on proposals
     @dev In order for a vote to be counted, an address must first register as a participant.
-    @dev The outcome will be determined offchain by tabulating the number of votes for each _vote and then weighting those by multiplying each _vote by the specified amout for each address in terms of their GreenhouseGasUsage.
+    @dev The outcome will be determined offchain by tabulating the number of votes for each _vote and then weighting those by multiplying each _vote by the specified weight for each address in terms of their GreenhouseGasUsage.
+    Ex, proposalID #3 has 2 votes from the charburger group:
+    2 * 45 = 90
     @param _vote Must match one of the options declared in the proposal, otherwise it is thrown out.
+    @dev The voting period limit will be enforced offchain.  The block.timestamp for a vote must be a value less than the proposal's block.timestamp + votingPeriod.
   **/
   function vote(uint256 _proposalId, uint256 _vote) external {
     require(_proposalId == true; "GasDAOn: proposalID does not exist");
